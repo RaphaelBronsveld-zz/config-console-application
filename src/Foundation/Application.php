9b7faa9406b39d879b06589149014f699f41108f
Raphaelbronsveld <r-raaf-r@live.nl>
2016-01-29 10:17:41 +0100

    Maakt niet uit wat.

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -8,28 +8,13 @@
 
 namespace Raphaelb\Foundation;
 
-
-use Illuminate\Config\Repository;
 use Illuminate\Container\Container;
-use Illuminate\Filesystem\Filesystem;
-
 
 
 class Application extends Container
 {
     public function __construct()
     {
-        $this->bind('artisan', function() {
-            return new Artisan;
-        });
-
-        $this->singleton('config', function() {
-            return new Repository;
-        });
-
-        $this->bind('filesystem', function() {
-            return new Filesystem;
-        });
 
     }
 
--- src/Foundation/Artisan.php
+++ src/Foundation/Artisan.php
@@ -9,12 +9,38 @@
 namespace Raphaelb\Foundation;
 
 
+use Illuminate\Config\Repository;
+use Illuminate\Filesystem\Filesystem;
 use Symfony\Component\Console\Application as SymfonyApplication;
 
+/**
+ * Inherits SymfonyApp
+ */
 class Artisan extends SymfonyApplication
 {
+
+    protected $app;
+
+    /**
+     * Artisan constructor.
+     */
     public function __construct()
     {
+
+
         parent::__construct();
+
+        $this->app->bind('artisan', function() {
+            return new Artisan;
+        });
+
+        $this->app->singleton('configuration', function() {
+            return new Repository;
+        });
+
+        $this->app->bind('filesystem', function() {
+            return new Filesystem;
+        });
     }
+
 }
